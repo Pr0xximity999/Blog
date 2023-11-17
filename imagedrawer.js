128372ff340fd8f1141b2b51f5e1fc01da6498c1
Pr0xximity999 <tomsomgamer999@gmail.com>
2023-11-17 21:08:35 +0100

    finally came to a final readout of the files

--- imagedrawer.js
+++ imagedrawer.js
@@ -1,32 +1,16 @@
-var num = 4;
+var images = [
+    'jiggy.gif',
+    'tf2naked.gif',
+    'eye.jpg',
+    'snugbugrug.jpg',
+]
 var div = document.getElementById('gifDrawer');
 
-console.log("\nFilenames in directory:"); 
-for (let i = 1; i < num + 1; i++) {
+images.forEach(image => {
     var img = new Image();
-    if(ImageExist('imageDrawer/' + i + '.gif'))
-    {
-        img.src = 'imageDrawer/' + i + '.gif';
-    }
-    else if(ImageExist('imageDrawer/' + i + '.jpg'))
-    {
-        img.src = 'imageDrawer/' + i + '.jpg';
-    }
-    else if(ImageExist('imageDrawer/' + i + '.png'))
-    {
-        img.src = 'imageDrawer/' + i + '.png';
-    }
+    img.src = 'imageDrawer/' + image;
     img.className = 'gifDrawerImages';
     div.appendChild(img);
-}
 
-function ImageExist(url) {
-    if(url){
-        var req = new XMLHttpRequest();
-        req.open('GET', url, false);
-        req.send();
-        return req.status==200;
-    } else {
-        return false;
-    }
-}
+});
+
