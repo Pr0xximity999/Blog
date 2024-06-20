f363e4b24f2676b4fd94d0976ca53bc65d4e31e6
Pr0xximity999 <tomsomgamer999@gmail.com>
2024-06-20 20:14:39 +0200

    iplogger take 5

--- server.js
+++ server.js
@@ -8,15 +8,29 @@ const port = settings.port;
 const staticHtmlPath = path.join(__dirname, './website');
 
 const app = express();
+
+function visitor (req, res, next) {
+    console.log(req.ip)
+    console.log(req.path)
+    console.log(req.method)
+ 
+    next();
+ }
+
+
 app.use(cors());
+app.use(visitor)
 app.use(expressStatic(staticHtmlPath));
+
 // start the Express server
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}...`);
 }).on('error', (err) => {
     console.error('Server startup error:', err);
 });
+
 app.get('/', function (req, res) {
-    console.log(req + " || connected to the site.");
+    var userIP = req.socket.remoteAddress;
+    console.log(userIP + " connected to the site.");
     res.send("I see you :)")
 });
