641f364c7f2c254890e948b2f105808be9d96fcb
Pr0xximity999 <tomsomgamer999@gmail.com>
2024-09-10 19:46:10 +0200

    jsons arent iterable if its only 1 item ._.

--- website/AvansRss/avansRss.js
+++ website/AvansRss/avansRss.js
@@ -6,15 +6,14 @@ const convert = require('xml-js');
 
 
 //Im lazy to type the dictionary lookup every time
-function GetData(dict, tag, tag2='_text')
-{
+function GetData(dict, tag, tag2 = '_text') {
     return dict[tag][tag2];
 }
 
 const rssRouter = express.Router();
 rssRouter.use(express.json())
 
-rssRouter.get('', (req, res) =>{
+rssRouter.get('', (req, res) => {
     res.status(200).sendFile(path.join(__dirname, 'avansrss.html'))
 })
 
@@ -26,39 +25,61 @@ rssRouter.get('/api', (req, res) => {
         .then(response => response.text())
         .then((xml) => {
 
-            fs.readFile('./index.html', 'utf8', (err, document) => {          
+
+            fs.readFile('./index.html', 'utf8', (err, document) => {
                 //Format the xml and grab the rss data
                 var json = JSON.parse(convert.xml2json(xml, {
                     compact: true
                 }))['rss'];
 
-
                 
                 var articles = [];
-                json['channel']['item'].forEach(a => {
+                try {
+                    json['channel']['item'].forEach(a => {
+                        var textTemplate = `<div id='AvansRss'>
+                            <div id='HeaderDiv_${GetData(a, 'guid')}'>
+                                    <div id='titleDiv'>
+                                    <h1 id='title'>${GetData(a, 'title')}</h1>
+                                    </div>
+                                    <div id='pubDateDiv'>
+                                    <span id='pubDate' data-value='${GetData(a, 'pubDate')}'>${GetData(a, 'pubDate')}</span>
+                                    </div>
+                                    </div>
+                                    <div id='descriptionDiv'>
+                                    ${GetData(a, 'description')}
+                                    </div>
+                                    <div id='linkDiv'>
+                                    <a href='${GetData(a, 'link')}', id='link'>${GetData(a, 'link')}</a>
+                                    </div>
+                                    </div>`
+                        textTemplate = textTemplate.replace(/style=".*"/gm, '')
+    
+                        articles.push(textTemplate)
+                    });
+                } catch (error) {
+                    a = json['channel']['item']
                     var textTemplate = `<div id='AvansRss'>
                             <div id='HeaderDiv_${GetData(a, 'guid')}'>
-                                <div id='titleDiv'>
+                                    <div id='titleDiv'>
                                     <h1 id='title'>${GetData(a, 'title')}</h1>
-                                </div>
-                                <div id='pubDateDiv'>
+                                    </div>
+                                    <div id='pubDateDiv'>
                                     <span id='pubDate' data-value='${GetData(a, 'pubDate')}'>${GetData(a, 'pubDate')}</span>
-                                </div>
-                            </div>
-                            <div id='descriptionDiv'>
-                                ${GetData(a, 'description')}
-                            </div>
-                            <div id='linkDiv'>
-                                <a href='${GetData(a, 'link')}', id='link'>${GetData(a, 'link')}</a>
-                            </div>
-                        </div>`
-                    textTemplate = textTemplate.replace(/style=".*"/gm, '')
-                    
-                    articles.push(textTemplate)
-                    });
-                    
+                                    </div>
+                                    </div>
+                                    <div id='descriptionDiv'>
+                                    ${GetData(a, 'description')}
+                                    </div>
+                                    <div id='linkDiv'>
+                                    <a href='${GetData(a, 'link')}', id='link'>${GetData(a, 'link')}</a>
+                                    </div>
+                                    </div>`
+                        textTemplate = textTemplate.replace(/style=".*"/gm, '')
+    
+                        articles.push(textTemplate)
+                }
                 var page = articles.join('')
-                    
+
                 //Insert the rss into the html
                 res.status(200).send(page);
             })
