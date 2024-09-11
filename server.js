e1f286635e03b18fd492647cda3862352a20b804
Pr0xximity999 <tomsomgamer999@gmail.com>
2024-09-11 17:09:13 +0200

    disabled the rss for legal reasons

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(cors());
 app.use(visitor)
 app.use(expressStatic(staticHtmlPath)); 
 
-app.use('/avansrss', rssRouter);
+// app.use('/avansrss', rssRouter);
 
 app.use((req, res, next) => { 
     res.status(404).sendFile(path.join(__dirname, './website/404.html'))
